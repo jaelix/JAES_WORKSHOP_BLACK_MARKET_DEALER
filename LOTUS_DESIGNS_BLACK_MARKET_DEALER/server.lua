e5c694e110d1f7d80fd433d9798878365b0f69d7
Jae's WorkShop <zacharyg741@gmail.com>
2025-07-17 05:23:19 -0400

    Delete LOTUS_DESIGNS_BLACK_MARKET_DEALER directory

--- LOTUS_DESIGNS_BLACK_MARKET_DEALER/server.lua
+++
@@ -1,5 +0,0 @@
-RegisterServerEvent("blackmarket:openMenu")
-AddEventHandler("blackmarket:openMenu", function()
-    local _source = source
-    TriggerClientEvent("blackmarket:showMenu", _source)
-end)
